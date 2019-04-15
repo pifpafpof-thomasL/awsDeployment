7cab6542523faa997f72e54407d139b245a7f02e
thomas <thomas.lechevalier@wildcodeschool.fr>
2019-04-15 16:19:19 +0200

    ready to deploy

---
+++ client/build/precache-manifest.b753591f26577fe3034fc0cd334dd55c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "44d1c487dc0b043dae98",
+    "url": "/static/js/main.8482b179.chunk.js"
+  },
+  {
+    "revision": "f94b5aaa894492841fbc",
+    "url": "/static/js/2.22483100.chunk.js"
+  },
+  {
+    "revision": "44d1c487dc0b043dae98",
+    "url": "/static/css/main.c96f0127.chunk.css"
+  },
+  {
+    "revision": "82962332e0170f6848a5d53ba7efb704",
+    "url": "/index.html"
+  }
+];
